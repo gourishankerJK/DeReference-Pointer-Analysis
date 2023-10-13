0fa5488564f17ccb32e7d310e69ac1354e12eca1
Gouri Shanker <gourishanker@Gouris-MacBook-Air.local>
2023-10-13 13:27:20 +0530

    Added Print Function for PLE

--- PointerLatticeElement.java
+++ PointerLatticeElement.java
@@ -21,27 +21,37 @@ public class PointerLatticeElement implements LatticeElement {
     public HashMap<String, Set<String>> getState() {
         return this.State;
     }
-     public void setState( HashMap<String, Set<String>> state) {
+
+    public void setState(HashMap<String, Set<String>> state) {
         this.State = state;
     }
-    
+
+    public void printState() {
+        System.out.print("{");
+        for (String key : this.State.keySet()) {
+            System.out.print("{" + key + ":{");
+            for (String value : this.State.get(key)) {
+                System.out.print(value + ",");
+            }
+            System.out.print("}} , \n");
+        }
+        System.out.print("}");
+    }
 
     @Override
     public LatticeElement join_op(LatticeElement r) {
-         HashMap<String , Set<String>>  input = ((PointerLatticeElement) r).getState() ;
+        HashMap<String, Set<String>> input = ((PointerLatticeElement) r).getState();
 
-         HashMap<String , Set<String>> joinElementState = new HashMap<String , Set<String>>(); 
+        HashMap<String, Set<String>> joinElementState = new HashMap<String, Set<String>>();
 
-         
-         for(String key : input.keySet()){
-             joinElementState.put(key, set_merge(this.State.get(key) , input.get(key)));  
+        for (String key : input.keySet()) {
+            joinElementState.put(key, set_merge(this.State.get(key), input.get(key)));
         }
-        PointerLatticeElement joinE = new PointerLatticeElement(); 
+        PointerLatticeElement joinE = new PointerLatticeElement();
         joinE.setState(joinElementState);
 
         return (LatticeElement) joinE;
 
-         
     }
 
     @Override
