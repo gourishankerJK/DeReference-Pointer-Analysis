27c7f838359273963d425a527d13d0b4f6e67247
Avinash Jayakar <avithemad@Dhananjayarajs-MacBook-Pro.local>
2023-10-16 12:34:34 +0530

    Move propagate logic to Kildall.java

--- PointerLatticePreProcess.java
+++ PointerLatticePreProcess.java
@@ -3,10 +3,10 @@ import java.util.HashMap;
 import java.util.List;
 
 import soot.Body;
+import soot.Local;
+import soot.RefType;
 import soot.Unit;
-import soot.ValueBox;
 import soot.jimple.Stmt;
-import soot.jimple.internal.JInstanceFieldRef;
 import soot.tagkit.LineNumberTag;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 
@@ -38,15 +38,10 @@ public class PointerLatticePreProcess implements IPreProcess {
 
     public static List<String> GetRefTypeVariables(Body body) {
         List<String> result = new ArrayList<String>();
-        for (Unit unit : body.getUnits()) {
-            for (ValueBox vBox : unit.getDefBoxes()) {
-                // only consider variables of the reference types -- ASSUMPTION
-                if (vBox.getValue().getType().getClass().equals(soot.RefType.class) &&
-                        !vBox.getValue().getClass().equals(JInstanceFieldRef.class)) {
-                    result.add(vBox.getValue().toString());
-                }
+        for (Local local : body.getLocals()) {
+            if (local.getType().getClass().equals(RefType.class)) {
+                result.add(local.toString());
             }
-
         }
         return result;
     }
