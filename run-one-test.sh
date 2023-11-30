44185d3a6059f0ecebc8e23e04bb2b99fa62efc8
Gouri Shanker <gourishanker@Gouris-MacBook-Air.local>
2023-11-30 14:14:07 +0530

    testing script change

--- run-one-test.sh
+++ run-one-test.sh
@@ -8,6 +8,7 @@ echo "Running java -Xms800m -Xmx3g Analysis $DIRECTORY  $CLASS  $CLASS  "$TARGET
 time \
     mkdir -p iterations
   find iterations ! -name '*.pdf' -type f -exec rm -f {} +
+   find $DIRECTORY ! -name '*.java' '*.class' -type f -exec rm -f {} +
     java -Xms800m -Xmx3g Analysis  $DIRECTORY  $CLASS  $CLASS "$TARGETMETHOD" test
 time \
     ./run-generate-pdf.sh $CLASS $TARGETMETHOD 
@@ -16,7 +17,7 @@ time \
 #!/bin/bash
 
 # Define the source folders
-source_folder1="target1-pub"
+source_folder1=$DIRECTORY
 source_folder2="expected-output"
 
 GREEN='\033[0;32m'
@@ -26,7 +27,7 @@ RESET='\033[0m'
 file_name="$CLASS.$TARGETMETHOD.output.txt"
 # echo "comparing actual-output/$file_name with expected-output/$file_name"
 if [ -e "$source_folder2/$file_name" ]; then
-    difference=$(diff "$source_folder1/$file_name" "$source_folder2/$file_name")
+    difference=$(diff --suppress-common-lines --side-by-side "$source_folder1/$file_name" "$source_folder2/$file_name")
     if [ -z "$difference" ]; then
 
         echo "${GREEN}Test $TARGETMETHOD passed${RESET}"
